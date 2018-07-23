9a0d96e8cf646b8c853de7767506220c2dd4172b
Victória Braia <victoriabraia@gmail.com>
2018-07-22 21:06:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int M, N;
+        public static Dictionary<String, String> tabLang = new Dictionary<string, string>();
+        public static List<String> childNames = new List<string>();
+        public static List<String> childLang = new List<string>();
+
+        static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string language = Console.ReadLine();
+                string merryChristmas = Console.ReadLine();
+                tabLang.Add(language, merryChristmas);
+            }
+            M = int.Parse(Console.ReadLine());
+            for (int i = 0; i < M; i++)
+            {
+                string name = Console.ReadLine();
+                string language = Console.ReadLine();
+                childNames.Add(name);
+                childLang.Add(language);
+            }
+
+
+            for (int i = 0; i < childNames.Count; i++)
+            {
+                string merryChristmas;
+                string key = childLang[i];
+                if (tabLang.TryGetValue(key, out merryChristmas))
+                {
+                    Console.WriteLine(childNames[i]);
+                    Console.WriteLine(merryChristmas);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
